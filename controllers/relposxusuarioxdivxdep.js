548e5282f42f05504ed63906495b9c14a7c0ee9e
ezexedge <ezeedge@gmail.com>
2021-08-05 03:28:33 -0300

    guardando cambios

--- controllers/relposxusuarioxdivxdep.js
+++ controllers/relposxusuarioxdivxdep.js
@@ -1,14 +1,34 @@
 const RelPosXUsarioXDiviXDep = require('../models/RelPosXUsarioXDiviXDep')
-
-
+const ClubXUsuario = require('../models/ClubXUsuario')
+const RelDisXClubXDiv = require('../models/RelDisXClubXDiv')
+const DisciplinaXClubXPos = require('../models/DisciplinaXClubXPos')
+const RelDisciplinaXPos = require('../models/RelDisciplinaXPos')
 exports.getAll = async (req,res) => {
     
     try{
 
      
 
-        const result = await RelPosXUsarioXDiviXDep.findAll({})
-        
+        const result = await RelPosXUsarioXDiviXDep.findAll({
+            include: [{
+                model: ClubXUsuario,
+                as: 'clubxusuario'
+            },
+            {
+                model: RelDisXClubXDiv,
+                as: 'disxclubxdiv'  
+            },
+            {
+             model: DisciplinaXClubXPos,
+             as:   'disciplinaxclubxpos',
+             include: [{
+                 model: RelDisciplinaXPos,
+                 as: 'disciplinaxpos'
+             }]
+            }
+        ]
+        })
+
      
 
         res.status(200).json(result)
@@ -17,3 +37,16 @@ exports.getAll = async (req,res) => {
         res.status(400).json({'error': error.message})
     }
 }
+
+
+/*
+RelPosXUsuarioXDivXDep.belongsTo(ClubXUsuario,{as:"clubxusuario", foreignKey: 'clubxusuarioId'})
+RelPosXUsuarioXDivXDep.belongsTo(RelDisXClubXDiv,{as:"disxclubxdiv",foreignKey: 'disxclubxdivId'})
+RelPosXUsuarioXDivXDep.belongsTo(DisciplinaXClubXPos,{as:"disciplinaxclubxpos", foreignKey: 'disciplinaxclubxposId'})
+
+
+
+DisciplinaXClubXPos.belongsTo(RelDisciplunaXClub,{as:"disxclub",foreignKey: 'disxclubId'})
+DisciplinaXClubXPos.belongsTo(RelDisciplinaXPos,{as:"disciplinaxpos", foreignKey: 'disciplinaxposId'})
+
+*/
