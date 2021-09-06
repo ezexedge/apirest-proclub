28153738e164da592ca2be9459760868d45acde2
ezequiel gallardo <ezeedge@gmail.com>
2021-09-06 16:17:41 -0300

    agregando valores por defaul a  modelo de configuracion

--- models/ConfiguracionDiasHs.js
+++ models/ConfiguracionDiasHs.js
@@ -11,39 +11,55 @@ const ConfiguracionDiasHs = db.define('configuraciondiashs', {
     },
     lunes:{
         type: Sequelize.INTEGER,
+        defaultValue: 0,
+
         allowNull: false
     },
     martes:{
         type: Sequelize.INTEGER,
+        defaultValue: 0,
+
         allowNull: false
     },
     miercoles:{
         type: Sequelize.INTEGER,
+        defaultValue: 0,
+
         allowNull: false
     },
     jueves:{
         type: Sequelize.INTEGER,
+        defaultValue: 0,
+
         allowNull: false
     },
     viernes:{
         type: Sequelize.INTEGER,
+        defaultValue: 0,
+
         allowNull: false
     },
     sabado:{
         type: Sequelize.INTEGER,
+        defaultValue: 0,
+
         allowNull: false
     },
     domingo:{
         type: Sequelize.INTEGER,
+        defaultValue: 0,
+
         allowNull: false
     },
     desde: {
         type: Sequelize.TIME,
+        defaultValue: '00:00:00',
 
         allowNull: false
     },
     hasta: {
         type: Sequelize.TIME,
+        defaultValue: '00:00:00',
 
         allowNull: false
     },
