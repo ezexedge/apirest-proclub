1e6bcff89b268ddd57f7d5ab26f11bf7b0d46cf4
ezexedge <ezeedge@gmail.com>
2021-10-01 07:45:40 -0300

    agregando estado en notificacion

--- models/Notificacion.js
+++ models/Notificacion.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const Rubro = require('./Rubro')
 const db = require('../config/db');
 const moment = require('moment')
+const EstadoNotificacion = require('../model/EstadoNotificacion')
 
 
 
@@ -39,4 +40,10 @@ const Notificacion = db.define('notificacion', {
 
 
 
+
+Notificacion.belongsTo(EstadoNotificacion,{as:"estadonotificacion",foreignKey: 'estadonotificacionId'})
+
+
+
 module.exports = Notificacion;
+
