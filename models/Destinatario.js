db7b1aec2ab2f41b24043bad74214b5c764c1755
ezexedge <ezeedge@gmail.com>
2021-12-02 11:56:03 -0300

    agregando cambios en el modelo de destinatarios

--- models/Destinatario.js
+++ models/Destinatario.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const Encuesta = require('../models/Encuesta')
 const Usuario = require('../models/Usuario')
+const Club = require('../models/Club')
 const db = require('../config/db');
 
 const Destinatario = db.define('destinatario', {
@@ -17,6 +18,7 @@ const Destinatario = db.define('destinatario', {
 Destinatario.belongsTo(Encuesta,{as:"encuesta",foreignKey: 'encuestId'})
 Destinatario.belongsTo(Usuario,{as:"usuario",foreignKey: 'usuarioId'})
 Destinatario.belongsTo(Usuario,{as:"enviadoPor",foreignKey: 'enviadoporId'})
+Destinatario.belongsTo(Club,{as:"club",foreignKey: 'clubId'})
 
 
 
