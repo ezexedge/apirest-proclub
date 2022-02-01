5b461353e82ca1c203775c6dbd12475af9312805
ezexedge <ezeedge@gmail.com>
2022-02-01 12:03:48 -0300

    ff

--- controllers/beneficios.js
+++ controllers/beneficios.js
@@ -35,7 +35,7 @@ exports.crear = async (req, res) => {
 
     const resultBeneficio = await Beneficios.create({ nombre: nombre, descripcion: descripcion, telefono: telefono , web : web , instagram: instagram , correo: correo, pathImage : `https://api.klubo.club/api/image/${imagen}` , pertenece_superadmin: 1, created: new Date()},{ transaction: t })
     
-    BeneficioXClub.create({clubId: null , usuarioId: null, beneficioId: beneficio,pertenece_superadmin: 1})
+     await  BeneficioXClub.create({clubId: null , usuarioId: null, beneficioId: resultBeneficio.id,pertenece_superadmin: 1})
     
     if(rubro.length > 0 ){
 
